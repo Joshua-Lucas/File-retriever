859b3e22b9ebd97943e6d3259b34e40fa6278089
Joshua Lucas <joshuamlucas91@gmail.com>
2021-04-17 21:07:05 -0400

    utils(errorHandling): Add error messages and function to log errors

---
+++ src/utils/fileErrors.ts
@@ -0,0 +1,5 @@
+export const fileErrors = {
+  WRONG_FILE_EXTENSION: "The file extension is acceptable",
+  FILE_DOES_NOT_EXISTS:
+    "There is no file with that name. File was either moved or you have the wrong name format in your paramter",
+};
