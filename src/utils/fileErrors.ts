3d322ce19e7a6c1e05f8e4c1499c20ff0b607170
Joshua Lucas <joshuamlucas91@gmail.com>
2021-04-19 00:58:04 -0400

    refactor: fixed error message

--- src/utils/fileErrors.ts
+++ src/utils/fileErrors.ts
@@ -1,5 +1,5 @@
 export const fileErrors = {
-  WRONG_FILE_EXTENSION: "The file extension is acceptable",
+  WRONG_FILE_EXTENSION: "file extension is not accepted",
   FILE_DOES_NOT_EXISTS:
     "There is no file with that name. File was either moved or you have the wrong name format in your paramter",
 };
